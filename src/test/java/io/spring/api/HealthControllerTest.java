cff2a9fc6b750bd3a04ffd182d97384f009b660f
omar-mohd <3omar.mohd@1utar.my>
2026-04-10 21:36:48 +0800

    add unit tests for health and info endpoints

---
+++ src/test/java/io/spring/api/HealthControllerTest.java
@@ -0,0 +1,26 @@
+package io.spring.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthEndpointShouldReturnRunningMessage() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Application is running"));
+    }
+}
---
+++ src/test/java/io/spring/api/InfoControllerTest.java
@@ -0,0 +1,25 @@
+package io.spring.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InfoControllerTest {   
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void infoEndpointShouldReturnData() throws Exception {   
+        mockMvc.perform(get("/info"))   
+                .andExpect(status().isOk());
+    }
+}
